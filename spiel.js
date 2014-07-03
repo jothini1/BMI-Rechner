5f859499650b7b631c6b3a988d70fd1954da59a0
Jothini <Jothini@192.168.2.106>
2014-07-03 19:12:31 +0200

    App fertig

--- spiel.js
+++ spiel.js
@@ -10,9 +10,23 @@ $("body").html("<br> Körpermasse in kg:  <input id='masse' type='text' size='10
             var groesse=parseInt($("#groesse").val());
            
             var bmi=parseInt(masse/((groesse/100)*(groesse/100)));
+           
             
-            $("body").append("Dein BMI beträgt "+bmi+".");
+            $("body").append("<div class='normalertext'> Dein BMI beträgt "+bmi+".</div>");
             
+            if (bmi<=18){
+            $("body").append("<br> <div class='normalertext'> Ein BMI unter 18 weist auf Untergewicht hin. Du \n\
+                 solltest mehr essen und Dir nicht zu viele Gedanken um dein Körpergewicht machen.</div>");
+             }else if (18<bmi<=24){
+                 $("body").append("<br> <div class='normalertext'> Du hast einen optimalen BMI, welcher auf\n\
+                      Normalgewicht hinweist. Mach weiter so :) </div>");
+             }else if (bmi>24){
+                 $("body").append("<br> <div class='normalertext'> Dein BMI ist größer als 24 und weist auf leichtes\n\
+                                    Übergewicht hin. Aber das Aussehen allein spielt keine Rolle! </div>");
+             }
+             
+             $("body").append("Das ist übrigens Jothini's erste App :) ");
+             
             });
              
 });
